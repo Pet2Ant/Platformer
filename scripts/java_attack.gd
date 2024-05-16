b7db41a363dd8b891fa5027b24bd16676cc0ea59
upgradedtofml <ckan1995@hotmail.com>
2024-05-16 03:05:24 +0300

    fix player phsyiccs

--- scripts/java_attack.gd
+++ scripts/java_attack.gd
@@ -21,3 +21,10 @@ func _on_area_2d_area_entered(area):
 	if area.get_parent() is Player && !hit:
 		area.get_parent().take_damage(1)
 		die()
+
+
+
+
+func _on_area_2d_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	if body is TileMap:
+		queue_free()
--- scripts/maincharacter.gd
+++ scripts/maincharacter.gd
@@ -17,15 +17,15 @@ var applied_impulse_force = 5
 var can_control : bool = true
 var force_timer = 0.0
 var force_duration = 0.5  
+var friction = 0.75
+var stop_friction = 5*friction
 @onready var sprite_2d = $AnimatedSprite2D
-@onready var animation_player =$AnimationPlayer
 var starting_position = Vector2(180,200)
 var max_health = 3
 var health = 0
 var can_take_damage = true;
 var speed_dir 
-var friction = 0.75
-var stop_friction = 5*friction
+
 func _ready():
 	health = max_health
 	GameManager.player = self
@@ -39,19 +39,17 @@ func _physics_process(delta):
 		coyote_timer += delta
 	else:
 		coyote_timer = 0.0
-	
 	var input_vector: Vector2 = Vector2.ZERO
 	# Handle input.
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector = input_vector.normalized()
-	
-	if input_vector.x != 0:
-		velocity.x = lerp(velocity.x, input_vector.x * SPEED, friction * delta)
-	else:
-		velocity.x = lerp(velocity.x, 0.0, stop_friction * delta)
-		
-		
-		
+	if is_on_floor():
+		if input_vector.x != 0:
+			velocity.x = lerp(velocity.x, input_vector.x * SPEED, friction * delta)
+		else:
+			velocity.x = lerp(velocity.x, 0.0, stop_friction * delta)
+  
+
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and (is_on_floor() or coyote_timer < COYOTE_TIME) :
 		velocity.y = JUMP_VELOCITY
@@ -98,19 +96,18 @@ func _physics_process(delta):
 func add_force(explosion_position : Vector2):
 	applied_impulse = true
 	var direction = (global_position.x - explosion_position.x)
-	print("direction",)
 	
 	if direction < 0:
 		if SPEED > 0:
-			speed_dir = 130
+			speed_dir = 80
 		elif SPEED < 0 :
-			speed_dir = -130
+			speed_dir = -80
 		velocity.x = -applied_impulse_force * speed_dir 
 	elif direction > 0:
 		if SPEED > 0:
-			speed_dir = 130
+			speed_dir = 80
 		elif SPEED < 0 :
-			speed_dir = -130
+			speed_dir = -80
 		velocity.x = applied_impulse_force * speed_dir 
 	
 func take_damage(damage_amount : int):
